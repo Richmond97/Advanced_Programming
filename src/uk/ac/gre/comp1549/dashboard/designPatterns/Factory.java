4da1d5dff7c5988bbe986504be6e98c4793d3116
H <mn7754c@gre.ac.uk>
2019-03-23 12:32:05 +0000

    - Wrote up code for Singleton and Factory design patterns

--- src/uk/ac/gre/comp1549/dashboard/designPatterns/Factory.java
+++ src/uk/ac/gre/comp1549/dashboard/designPatterns/Factory.java
@@ -1,7 +1,24 @@
-
 package uk.ac.gre.comp1549.dashboard.designPatterns;
 
 //Factory design pattern class
-public class Factory { //This could be used to decide which class gets called based on the string passed to it
-    
+import uk.ac.gre.comp1549.dashboard.barPanels.BarPanel;
+import uk.ac.gre.comp1549.dashboard.controls.MainPanel;
+import uk.ac.gre.comp1549.dashboard.digitalDials.DigitalBarPanel;
+import uk.ac.gre.comp1549.dashboard.fullDials.DialPanel;
+import uk.ac.gre.comp1549.dashboard.halfDials.HalfDialPanel;
+
+public class Factory { //This could be used to decide which dial class gets an instance returned to it by run time maybe
+
+    public static MainPanel createDial(String type) {
+        if (type.equals("SPEED") || type.equals("MILES")) {
+            return new DialPanel();
+        } else if (type.equals("TEMPERATURE")) {
+            return new HalfDialPanel();
+        } else if (type.equals("OIL")) {
+            return new DigitalBarPanel();
+        } else if (type.equals("PETROL")) {
+            return new BarPanel();
+        }
+        return null;
+    }
 }
--- src/uk/ac/gre/comp1549/dashboard/designPatterns/Singleton.java
+++ src/uk/ac/gre/comp1549/dashboard/designPatterns/Singleton.java
@@ -1,5 +1,19 @@
 package uk.ac.gre.comp1549.dashboard.designPatterns;
 
 public class Singleton { //Singleton design pattern class
+    private static Singleton instance = null;
+    
+    private Singleton(){}
+    
+    public static synchronized Singleton getInstance(){
+     if (instance == null){
+         instance = new Singleton();
+     }   
+     return instance;
+    }
+    
+    
+    
+    
     
 }
